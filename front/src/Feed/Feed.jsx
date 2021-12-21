b798a82f1274eef50f7cb55fc173ec297e671ed2
Joseph <joseph.vidal@epitech.eu>
2021-12-21 17:42:57 +0100

    [MOD] Feed : getting tweet list from server

--- front/src/Feed/Feed.jsx
+++ front/src/Feed/Feed.jsx
@@ -1,9 +1,47 @@
-import "./Feed.css"
+import { Component } from "react";
 
-export default function Feed() {
+import Tweet from "../Tweet/Tweet";
+import "./Feed.css";
+
+class Feed extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tweets: []
+    };
+    this.getTweets = this.getTweets.bind(this);
+    this.getTweets();
+  }
+
+  componentDidMount() {
+    this.data = this.getTweets();
+  }
+
+  getTweets() {
+    // this.setState({tweets: [
+    //   {author: "Jacques", content: "Coucou"},
+    //   {author: "Kenobi", content: "Hello there"},
+    //   {author: "Grivous", content: "GENERAL KENOBI"},
+    //   {author: "Charles de Gaulle", content: "Je vous ai comrpis."}
+    // ]});
+    fetch("/twits")
+    .then((response) => {
+      this.setState(this.state.tweets, response);
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Feed</h1>
-        </div>
+      <div className="feed-page">
+        <h1>Galiler</h1>
+        {
+          this.state.tweets.map(function(obj, i) {
+            return <Tweet key={i} name={obj.author} content={obj.content}/>
+          })
+        }
+      </div>
     );
+  }
 }
+
+export default Feed;
