c00bcf9a3cd703c617c2eeeaea041c742a585843
Benjamin Feller <benjamin.feller@epitech.eu>
2021-12-24 20:10:00 +0100

    refactor(users): add password, email and required annotations to user class

--- back/api/Models/User.cs
+++ back/api/Models/User.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TwitterApi.Models
 {
     public class User
     {
+        [Required]
         public String Name { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+
+        [Required]
+        public String Email { get; set; }
     }
 }
