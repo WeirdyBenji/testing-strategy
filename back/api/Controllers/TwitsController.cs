80bed0ec909b3cd751f7999be66bf6faa4c7892b
Leyton Ly <leyton.ly@epitech.eu>
2021-12-25 23:24:22 +0100

    test (twits): Get twits should return array

---
+++ back/api/Controllers/TwitsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TwitterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<AnyType> Get()
+        {
+            return new AnyType[] {};
+        }
+    }
+}
---
+++ back/tests/TwitsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TwitterApi.Controllers;
+
+namespace ApiTests
+{
+    [TestClass]
+    public class TwitsTests
+    {
+        [TestMethod]
+        public void GetTwits_ShouldReturnArray()
+        {
+            TwitsController twitsController = new TwitsController();
+            var actual = twitsController.Get();
+            Assert.IsInstanceOfType(actual, typeof(Array));
+        }
+    }
+}
