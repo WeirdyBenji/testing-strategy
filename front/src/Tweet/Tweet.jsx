62c3289e8fade0c4ab337810b5163968cc6f10bc
Benjamin Feller <benjamin.feller@epitech.eu>
2021-12-28 08:40:49 +0100

    feat(front): add fallback anonymous values

--- front/src/Tweet/Tweet.jsx
+++ front/src/Tweet/Tweet.jsx
@@ -3,8 +3,8 @@ import "./Tweet.css";
 export default function Tweet(props) {
   return (
     <div className="tweet">
-      <h2>{props.name}</h2>
-      <p>{props.content}</p>
+      <h2>{props.name || 'Anonymous'}</h2>
+      <p>{props.content || '...'}</p>
     </div>
   );
 }
