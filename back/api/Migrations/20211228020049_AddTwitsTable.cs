dc24266fef8a4fb4d83f442c75f3f76c89f56f3f
Benjamin Feller <benjamin.feller@epitech.eu>
2021-12-28 08:34:25 +0100

    feat(twit): add migration and upd model

---
+++ back/api/Migrations/20211228020049_AddTwitsTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitterApi.Migrations
+{
+    public partial class AddTwitsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Twit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Body = table.Column<string>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Twit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Twit_User_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Twit_AuthorId",
+                table: "Twit",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Twit");
+        }
+    }
+}
--- back/api/Models/Twit.cs
+++ back/api/Models/Twit.cs
@@ -1,6 +1,16 @@
-﻿namespace TwitterApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterApi.Models
 {
     public class Twit
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+        public User Author { get; set; }
+
+        public Twit[] Childs;
+        public Twit Parent;
     }
 }
